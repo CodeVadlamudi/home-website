979bd213061458bd33668524f3778f19317f64a2
CodeVadlamudi <codevadlamuid@gmail.com>
2023-12-24 18:37:23 +0530

    added text

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
           className="h-[650px] relative rounded-ss-[100px] rounded-ee-[100px] flex items-center justify-center p-5"
         >
           <div className="bg-white/80 p-10 space-y-4 max-w-screen-sm rounded-ss-[50px] rounded-ee-[50px]">
-            <h2 className="text-2xl md:text-3xl">
+            <h2 className="text-3xl md:text-3xl">
               Let's make your home beautiful together!
             </h2>
             <p>
